1fda3e849867ab4441f5e58cd935854bbb6b7563
jadrayton <jdrayton@mit.edu>
2022-03-14 16:29:22 -0400

    Data visualization sample

---
+++ source/abundance_plot.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sb
+import sys, getopt
+import os
+from IPython.display import display
+
+def main(argv):
+    inputfiles = []
+    outputfile = ''
+    try:
+        opts, args = getopt.getopt(argv,"i:o:")
+    except getopt.GetoptError:
+        print('abundance_plot.py -i <inputfiles> -o <outputfile>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == "-i":
+            inputfiles.append(arg)
+        elif opt == "-o":
+            outputfile = arg
+    sample_names = []
+    for file in inputfiles:
+        if file[:30] == 'salmon_harmonized_transcripts_':
+            sample = file[30:]
+            sample = sample[:-4]
+            sample_names.append(sample)
+        else:
+            sample_names.append(file[:-4])
+    sample_dict = dict(zip(sample_names, inputfiles))
+    abundance_df = pd.DataFrame(columns=sample_names)
+    os.chdir('../processed')
+    for sample in sample_names:
+        data = np.loadtxt(sample_dict[sample], skiprows=1, usecols=-1)
+        abundance_df.loc[:,sample]=data
+    ax = abundance_df.plot.hist(bins=10, logy=True, ylim=(1,5*10**5), alpha=0.5)
+    ax.set_xlabel('Counts')
+    os.chdir('../plots')
+    plt.savefig(outputfile)
+    plt.show()
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
